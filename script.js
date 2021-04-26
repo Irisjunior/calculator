f45eab36f823413ff1fcc8091ce519b4f3f43217
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-04-26 18:49:31 -0300

    layout

---
+++ script.js
@@ -0,0 +1,10 @@
+var numero = document.querySelector('#resultado')
+
+function insert(num) {
+  numero.innerHTML=num
+  document.querySelector('#resultado') = numero + num
+}
+
+function clean() {
+  a.innerHTML = ''
+}
