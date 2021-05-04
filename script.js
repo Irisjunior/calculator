f2cbefb9409a9f9e4f612c8719c970f46923c42f
Iris Leite Vieira Junior <irisjunior1@gmail.com>
2021-05-04 20:09:45 -0300

    insercao do javascrip e modo noturno

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-var numero = document.querySelector('#resultado')
+
 
 function insert(num) {
-  numero.innerHTML=num
-  document.querySelector('#resultado') = numero + num
+ var numero = document.getElementById('res').innerHTML;
+              document.getElementById('res').innerHTML= numero + num;
+  
 }
 
-function clean() {
-  a.innerHTML = ''
+function apagar() {
+  document.getElementById('res').innerHTML = ' '
 }
+
+function back() {
+  var res = document.getElementById('res').innerHTML;
+  document.getElementById('res').innerHTML= res.substring(0,res.length - 1)
+}
+
+
+
+function calcular() {
+  var res = document.getElementById('res').innerHTML;
+  if (res) {
+    document.getElementById('res').innerHTML = eval(res);
+  } else {
+    
+  }
+}
+
+function light() {
+getElementById('container').style.backgroundColor='LightGray';
+  
+}
+
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#light-mode')
+
+$checkbox.addEventListener('change',function () {
+  $html.classList.toggle('light-mode')
+})
